695c59fb677d8483009e9f07852d1ef213a5cefd
vscilab <elvisseveriano2252@gmail.com>
2019-11-18 10:41:19 -0800

    new commit

--- src/csv30.cc
+++ src/csv30.cc
@@ -14,7 +14,7 @@ using std::vector;
 
 vector<int> merge_sorted(vector<int> a, vector<int>b) {
     vector<int> merged;
-    for(idxA = 0, idxB = 0; idxA < a.size() || idxB < b.size(); ){
+    for(int idxA = 0, idxB = 0; idxA < a.size() || idxB < b.size(); ){
         (idxA <= idxB) ? (merged.push_back(a[idxA]), idxA++) : (merged.push_back(b[idxB]), ++idxB);
     }
 }
